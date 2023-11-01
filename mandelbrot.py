78a7443b5c1bcfb1cd3dc0b6d550513e6c2c44dc
Foundnation <58121347+Foundnation@users.noreply.github.com>
2023-11-01 12:55:36 +0100

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -39,23 +39,32 @@ def generate_mandelbrot(z0, c_array, iterations, c_threshold, heights = False):
 
         return mandelbrot_set
         
-        
+def main():
+    c_shape = (1, int(1E6))
+    left_bound, right_bound = -2, 1
+    bottom_bound, top_bound = -1, 1
+    c_arr = (np.random.uniform(left_bound, right_bound, c_shape) + 1.j * np.random.uniform(bottom_bound, top_bound, c_shape))[0]
 
-c_shape = (1, int(1E6))
-c_arr = (np.random.uniform(-2, 1, c_shape) + 1.j * np.random.uniform(-1, 1, c_shape))[0]
+    mand_set, colormap_complex = generate_mandelbrot(0, c_arr, 100, 4, heights=True)
 
-mand_set, colormap_complex = generate_mandelbrot(0, c_arr, 100, 4, heights=True)
+    x = [elem.real for elem in mand_set]
+    y = [elem.imag for elem in mand_set]
+    colormap = [modulo(elem) for elem in colormap_complex]
 
-x = [elem.real for elem in mand_set]
-y = [elem.imag for elem in mand_set]
-colormap = [modulo(elem) for elem in colormap_complex]
+    print(len(c_arr))
+    print(len(mand_set))
 
-print(len(c_arr))
-print(len(mand_set))
-print(len(mand_set) / len(c_arr))
+    S_rect = abs(right_bound - left_bound) * abs(top_bound - bottom_bound)
+    print('area = ', len(mand_set) / len(c_arr) * S_rect)
 
 
-cmap = plt.get_cmap('viridis')
-colors = cmap(colormap)
-plt.scatter(x, y, c=colors, s=0.1)
-plt.show()
+    cmap = plt.get_cmap('viridis')
+    colors = cmap(colormap)
+    plt.scatter(x, y, c=colors, s=0.1)
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    main()
