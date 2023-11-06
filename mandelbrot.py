5eb71dce3739d7768d02c0c755fc66f64b2698f3
noahknijff <noahadam.knijff@gmail.com>
2023-11-06 16:25:59 +0100

    iteration bins

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,15 +5,20 @@ import time
 
 # ? change name cause it's not necessarily mandelbrot result?
 @jit
-def mandelbrot_func(z0, c, iterations, sequence=False):
+def mandelbrot_func(z0, c, iterations, iteration_bins, sequence=False):
     k = 0
     z = [z0]
     while k < iterations:
         z.append(z[k] * z[k] + c)
         k += 1
 
+    z_bins = []
     if sequence:
-        return z
+        z_step = int(len(z)/iteration_bins)
+        for i in range(iteration_bins):
+            z_bins.append(z[i * z_step])
+
+        return z_bins
     else:
         return z[-1]
 
@@ -22,13 +27,13 @@ def modulo(z):
     return np.sqrt((z.real)*(z.real) + (z.imag)*(z.imag))
 
 @jit
-def generate_mandelbrot(z0, c_array, iterations, z_threshold, heights = False):
+def generate_mandelbrot(z0, c_array, iterations, iteration_bins, z_threshold, heights = False):
     mandelbrot_set = []
     heightmap = []
 
     if heights:
         for c in c_array:
-            f = mandelbrot_func(z0, c, iterations)
+            f = mandelbrot_func(z0, c, iterations, iteration_bins, sequence=False)
             if modulo(f) < z_threshold:
                 mandelbrot_set.append(c)
                 heightmap.append(f)
@@ -37,7 +42,7 @@ def generate_mandelbrot(z0, c_array, iterations, z_threshold, heights = False):
     
     else:
         for c in c_array:
-            f = mandelbrot_func(z0, c, iterations)
+            f = mandelbrot_func(z0, c, iterations, iteration_bins, sequence=False)
             if modulo(f) < z_threshold:
                 #print(modulo(f))
                 mandelbrot_set.append(c)
@@ -50,7 +55,7 @@ def main():
     bottom_bound, top_bound = -1, 1
     c_arr = (np.random.uniform(left_bound, right_bound, c_shape) + 1.j * np.random.uniform(bottom_bound, top_bound, c_shape))[0]
 
-    mand_set, colormap_complex = generate_mandelbrot(0, c_arr, 100, 4, heights=True)
+    mand_set, colormap_complex = generate_mandelbrot(0, c_arr, 100, 10, 4, heights=True)
 
     x = [elem.real for elem in mand_set]
     y = [elem.imag for elem in mand_set]
