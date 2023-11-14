436c44c1da7b617c96041f04eb405924511be6fd
noahknijff <noahadam.knijff@gmail.com>
2023-11-14 13:56:17 +0100

    iterations vs sample size

--- mandelbrot.py
+++ mandelbrot.py
@@ -88,8 +88,7 @@ def complex_random_array(length, left_bound = -2, right_bound = 1, bottom_bound 
 
 """ *** Test Section *** """
 
-def test_convergence(sample_size):
-    num_samples = 10
+def test_convergence(sample_size, num_samples, iterations, iteration_step):
     area_array = np.zeros((num_samples))
     for i in range(num_samples):
         c_shape = (1, sample_size)
@@ -97,13 +96,36 @@ def test_convergence(sample_size):
         bottom_bound, top_bound = -1, 1
         c_arr = (np.random.uniform(left_bound, right_bound, c_shape) + 1.j * np.random.uniform(bottom_bound, top_bound, c_shape))[0]
 
-        mand_set = generate_mandelbrot(0, c_arr, 100, 10, 4)
+        mand_set = generate_mandelbrot(0, c_arr, iterations, iteration_step, 2, sequence=False, heights=False)
         area = calculate_area(left_bound, right_bound, bottom_bound, top_bound, len(mand_set), len(c_arr))
         area_array[i] = area
 
-    plt.plot([i for i in range(num_samples)], area_array)
-    plt.show()
+    # plt.plot([i for i in range(num_samples)], area_array)
+    # plt.show()
 
+    return sample_size, np.mean(area_array), np.std(area_array)
+
+
+def iterations_vs_no_samples(sample_size, num_samples_list, iterations_list):
+    area_array = np.zeros((len(num_samples_list), len(iterations_list)))
+    area_std_array = np.zeros((len(num_samples_list), len(iterations_list)))
+
+    for i, num_samples in enumerate(num_samples_list):
+        for j, num_iterations in enumerate(iterations_list):
+            area_temp = np.zeros((num_samples))
+            for k in range(num_samples):
+                c_shape = (1, sample_size)
+                left_bound, right_bound = -2, 1
+                bottom_bound, top_bound = -1, 1
+                c_arr = (np.random.uniform(left_bound, right_bound, c_shape) + 1.j * np.random.uniform(bottom_bound, top_bound, c_shape))[0]
+                mand_set = generate_mandelbrot(0, c_arr, num_iterations, 1, 2, sequence=False, heights=False)
+                area_temp[k] = calculate_area(left_bound, right_bound, bottom_bound, top_bound, len(mand_set), len(c_arr))
+            area_mean = np.mean(area_temp)
+            area_std = np.std(area_temp)
+            area_array[i][j] = area_mean
+            area_std_array[i][j] = area_std
+    return area_array, area_std_array
+    
 
 def plot_mandelbrot():
     c_shape = (1, int(1E5))
@@ -151,7 +173,33 @@ def intermediate_iterations_test():
     print(lengths)
 
 def main():
-    intermediate_iterations_test()
+    # intermediate_iterations_test()
+    num_samples_list = [i for i in range(10, 101, 20)]
+    iterations_list = [i for i in range(10, 101, 20)]
+
+    area_array, area_std_array = iterations_vs_no_samples(int(1E4), num_samples_list, iterations_list)
+
+    # Calculate step sizes
+    sample_step = num_samples_list[1] - num_samples_list[0]
+    iteration_step = iterations_list[1] - iterations_list[0]
+
+    plt.imshow(area_array, origin='lower', cmap='Greys', extent=[min(iterations_list)-iteration_step/2, max(iterations_list)+iteration_step/2, min(num_samples_list)-sample_step/2, max(num_samples_list)+sample_step/2])
+    plt.colorbar()
+    plt.xlabel('Iterations')
+    plt.ylabel('Num Samples')
+    plt.title('Area Array')
+    plt.show()
+
+    # Calculate step sizes
+    sample_step = num_samples_list[1] - num_samples_list[0]
+    iteration_step = iterations_list[1] - iterations_list[0]
+
+    plt.imshow(area_std_array, origin='lower', cmap='Greys', extent=[min(iterations_list)-iteration_step/2, max(iterations_list)+iteration_step/2, min(num_samples_list)-sample_step/2, max(num_samples_list)+sample_step/2])
+    plt.colorbar()
+    plt.xlabel('Iterations')
+    plt.ylabel('Num Samples')
+    plt.title(r'$\sigma(A)$')
+    plt.show()
 
 
 
@@ -159,3 +207,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# data2 = test_convergence(1000, 100, 100, 10)
+# data3 = test_convergence(2000, 100, 100, 10)
+# data4 = test_convergence(5000, 100, 100, 10)
+# data5 = test_convergence(10000, 100, 100, 10)
+# data6 = test_convergence(15000, 100, 100, 10)
+# data7 = test_convergence(20000, 100, 100, 10)
+# data8 = test_convergence(50000, 100, 100, 10)
+# data9 = test_convergence(100000, 100, 100, 10)
+
+
+
+# # convergence_data = [data2, data3, data4]
+
+# convergence_data = [data2, data3, data4, data5, data6, data7, data8, data9]
+
+# for data in convergence_data:
+#     sample_size = data[0]
+#     area_mean = data[1]
+#     area_std = data[2]
+#     plt.plot(sample_size, area_mean, 'bo', label=f'N = {sample_size}')
+#     plt.errorbar(sample_size, area_mean, yerr = area_std)
+
+# plt.xlabel('N')
+# plt.ylabel('A')
+# plt.show()
+
